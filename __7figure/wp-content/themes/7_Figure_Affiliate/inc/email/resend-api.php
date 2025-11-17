5e18d7b3e8012037b7c1a3bb2e2f0d03318375fb
Tevis Johnston <tevisjohnston@gmail.com>
2025-11-17 11:47:39 -0800

    Added directorys inc/ email/ and added file resend-api.php.

---
+++ __7figure/wp-content/themes/7_Figure_Affiliate/inc/email/resend-api.php
@@ -0,0 +1,12 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+$resend = Resend::client('re_Nvv8qc37_NRpP6A33LYwcX4faw1oewmVH');
+
+$resend->emails->send([
+  'from' => 'Acme <onboarding@resend.dev>',
+  'to' => ['delivered@resend.dev'],
+  'subject' => 'hello world',
+  'html' => '<strong>it works!</strong>',
+]);
